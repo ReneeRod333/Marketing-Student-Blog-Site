dc7291d8934410a8a153e23ee3de749292775b09
Renee Rodriguez <poulin.reneemarie@gmail.com>
2024-04-07 15:47:45 -0400

    Finished storing blogs into local storage and implemented display of blogs from local strage.

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,5 +1,30 @@
 
 
-  
+const mainEL = document.querySelector('#main');
+function renderLastRegistered() {
+
+    let blogs = [];
+    if (localStorage.getItem('blogs')) {
+        blogs = JSON.parse(localStorage.getItem('blogs'))
+    }
+    console.log(blogs.length)
+
+    for (const blog of blogs) {
+        console.log({ blog })
+        const usernameEl = document.createElement('span');
+        const usertitleEl = document.createElement('h3');
+        const usercontentEl = document.createElement('p');
+        const articleEl = document.createElement('article');
+        usernameEl.textContent = blog.username;
+        usertitleEl.textContent = blog.title;
+        usercontentEl.textContent = blog.content;
+        articleEl.appendChild(usertitleEl);
+        articleEl.appendChild(usercontentEl);
+        articleEl.appendChild(usernameEl);
+        mainEL.appendChild(articleEl);
+    }
+}
+renderLastRegistered();
+
 
 
---
+++ assets/js/form.js
@@ -0,0 +1,40 @@
+const usernameInput = document.querySelector('#uname');
+const titleInput = document.querySelector('#title');
+const contentInput = document.querySelector('#content');
+const submitButton = document.querySelector('#submit');
+const msgDiv = document.querySelector('#msg');
+const usernameSpan = document.querySelector('#user-name');
+const usertitleSpan = document.querySelector('#user-title');
+const usercontentSpan = document.querySelector('#blog-content');
+
+function displayMessage(type, message) {
+  msgDiv.textContent = message;
+  msgDiv.setAttribute('class', type);
+}
+
+submitButton.addEventListener('click', function (event) {
+  event.preventDefault();
+
+  const username = usernameInput.value;
+  const title = titleInput.value;
+  const content = contentInput.value;
+
+  if (username === '') {
+    displayMessage('error', 'User name cannot be blank');
+  } else if (title === '') {
+    displayMessage('error', 'Title cannot be blank');
+  } else if (content === '') {
+    displayMessage('error', 'Content cannot be blank');
+  } else {
+    displayMessage('success', 'Registered successfully');
+
+    let blogs = [];
+    if (localStorage.getItem('blogs')) {
+      blogs = JSON.parse(localStorage.getItem('blogs'))
+    }
+
+    const bloginfo = { username, title, content };
+    blogs.push(bloginfo);
+    localStorage.setItem('blogs', JSON.stringify(blogs));
+  }
+});
